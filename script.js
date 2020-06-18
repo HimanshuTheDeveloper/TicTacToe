a817b559032dc3915a9556b6d2339ac9a62cb5da
Himanshu <himanshu32301@gmail.com>
2020-06-18 17:20:48 +0530

    Diagonal checking logic

--- script.js
+++ script.js
@@ -11,10 +11,11 @@ col.forEach(cell => {
     cell.addEventListener('click', function(){
         if(cell.innerText == "")
         {
-            console.log(cell.id);
+            // console.log(cell.id);
             filled[cell.id - 1] = true;
             cell.innerText = userChoice;
             computerTurn();
+            
             // valueAccepter.classList.add("mystyle");
             // console.log(valueAccepter.classList.add("visible"));
 
@@ -24,7 +25,48 @@ col.forEach(cell => {
     });
 });
 
+function checkWinner()
+{
 
+    console.log("checking");
+    checkDiagonal();
+}
+
+function checkDiagonal() {
+
+    let dg1 = [
+                col[0].innerText, 
+                col[4].innerText, 
+                col[8].innerText
+            ];    
+    let dg2 = [
+                col[2].innerText, 
+                col[4].innerText, 
+                col[6].innerText
+            ];     
+
+    if(dg1[0] != "" && dg1[1] != "" && dg1[2] != "" )
+    {
+        if(dg1[0] == dg1[1] && dg1[0] == dg1[2])
+        {
+            
+            console.log("You win");
+            return true;
+        }
+    }
+    if(dg2[0] != "" && dg2[1] != "" && dg2[2] != "" )
+    {
+        if(dg2[0] == dg2[1] && dg2[0] == dg2[2])
+        {
+            console.log("You win");
+            return true;
+        }
+    }
+    else
+    {
+        return false;
+    }
+}
 
 
 function computerTurn()
@@ -45,7 +87,8 @@ function computerTurn()
     {
         col[computerSelected].innerText = 'X';
         filled[computerSelected] = true;
-        console.log(unfilled);
+        // console.log(unfilled);
+        checkWinner();
     }, 500);
 
 
